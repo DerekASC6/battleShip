3cda0720007230a53bf68e1d48bd0edaba1fd166
DerekASC6 <52749574+DerekASC6@users.noreply.github.com>
2019-07-18 14:54:40 -0400

    Add files via upload

---
+++ space.js
@@ -0,0 +1,14 @@
+function setup(){
+    createCanvas(900,900);
+    background(255,255,255);
+}
+
+let x;
+let y;
+function draw(){
+    rect (400,800,400,300);
+
+    circle (400,100,100);
+    square (400,200,100);
+    
+}
